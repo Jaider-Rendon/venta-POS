dbdb7a55adc302d78283a2ba8b5787e127d9f748
JFCP012 <juanfecelprieto@gmail.com>
2025-04-02 20:48:15 -0500

    Registro

---
+++ frontEND/src/app/entidad/cliente.ts
@@ -0,0 +1,10 @@
+export class Cliente {
+    cedulaC:string;
+    nombre1:string;
+    nombre2:string;
+    apellido1:string;
+    apellido2:string;
+    telefono:number;
+    correo:string;
+    direccion:string;
+}
