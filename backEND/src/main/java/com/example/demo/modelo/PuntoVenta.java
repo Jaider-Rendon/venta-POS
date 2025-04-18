e8e0e9e105abade318462958b7533d77b2128851
alexisunu <wilforalexis07@gmail.com>
2025-04-18 15:42:56 -0500

    agruege una clase para hacer los registros de salida de las facturas att:alexis

---
+++ backEND/src/main/java/com/example/demo/modelo/PuntoVenta.java
@@ -0,0 +1,65 @@
+package com.example.demo.modelo;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PuntoVenta")
+public class PuntoVenta {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "idPos")
+	    private Long idPos;
+
+	    @JsonFormat(pattern = "yyyy-MM-dd")
+	    @Column(name = "fechaVenta")
+	    private Date fechaVenta;
+
+	    @Column(name = "cantidadVendido")
+	    private int cantidadVendido;
+
+		public PuntoVenta() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public PuntoVenta(Date fechaVenta,  int cantidadVendido) {
+			super();
+			this.fechaVenta = fechaVenta;
+			this.cantidadVendido = cantidadVendido;
+		}
+
+		public Long getIdPos() {
+			return idPos;
+		}
+
+
+		public Date getFechaVenta() {
+			return fechaVenta;
+		}
+
+		public void setFechaVenta(Date fechaVenta) {
+			this.fechaVenta = fechaVenta;
+		}
+
+
+
+		public int getCantidadVendido() {
+			return cantidadVendido;
+		}
+
+		public void setCantidadVendido(int cantidadVendido) {
+			this.cantidadVendido = cantidadVendido;
+		}
+
+
+}
